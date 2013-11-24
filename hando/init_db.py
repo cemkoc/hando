75e4086dc22ffa4f64770021c432e441e2fa9b33
Brian Su <briansudev@gmail.com>
2013-11-23 22:59:50 -0800

    added database files

--- hando/init_db.py
+++ hando/init_db.py
@@ -1,7 +1,6 @@
 import sqlite3
 conn = sqlite3.connect('hando.db')
 c = conn.cursor()
-c.execute("CREATE TABLE hando (username, password, locks)")
-c.execute("INSERT INTO hando VALUES ('Smith Lock', 'password', 1)")
+c.execute("CREATE TABLE hando (username, password)")
 conn.commit()
 conn.close()
