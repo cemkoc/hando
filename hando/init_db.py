2f8230a364ef3ebb15917a11d237211e741d26e3
gh2o <git@thegavinli.com>
2013-11-24 08:58:24 -0800

    remove db

--- hando/init_db.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-from flask import Flask, g
-
-conn = sqlite3.connect('hando.db')
-c = conn.cursor()
-c.execute("CREATE TABLE hando (username, password)")
-conn.commit()
-conn.close()
