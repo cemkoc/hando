69d07a1110b3e1d51d91394834527168e43cce64
gh2o <git@thegavinli.com>
2013-11-23 20:52:20 -0800

    replace main with server

--- hando/main.py
+++ hando/main.py
@@ -1,9 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
+door_is_open = True
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug = True)
+def is_door_opened():
+	return not door_is_open
+
+@app.route('/push', methods=['GET', 'POST'])
+def push():
+	global door_is_open
+	print(door_is_open)
+	door_is_open = is_door_opened()
+	return render_template('push.html', door_is_open = door_is_open)
+
+if __name__ == "__main__":
+    app.run(debug=True)
